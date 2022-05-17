3c2c6773af528189ad63ca232b02369a395c5c43
Novik Dmitry <vanilla-cvety-krd@yandex.ru>
2022-05-17 18:15:19 +0300

    исправлены замечания после 1 проверки

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
+
 // кнопка редактирования данных
 const btnInfoUser = document.querySelector('.profile__info-button');
 // ------------------
 // форма пользователя
-const formPopapUser = document.querySelector('.popup_edit');
-const btnCloseFormUser = formPopapUser.querySelector('.popup__container-close');
-const formUserInfo = formPopapUser.querySelector('.popup__form');
-const nameUser = formPopapUser.querySelector('.popup__form-input_type_name');
-const discriptionUser = formPopapUser.querySelector('.popup__form-input_type_discription');
-const btnSaveFormUser = formPopapUser.querySelector('.popup__form-save-btn');
+const PopapUser = document.querySelector('.popup_edit');
+const btnCloseFormUser = PopapUser.querySelector('.popup__container-close');
+const formUserInfo = PopapUser.querySelector('.popup__form');
+const nameUser = PopapUser.querySelector('.popup__form-input_type_name');
+const discriptionUser = PopapUser.querySelector('.popup__form-input_type_discription');
+const btnSaveFormUser = PopapUser.querySelector('.popup__form-save-btn');
 // ------------------
 // доступы к попап открытия картинки
 const popupOpenImag = document.querySelector('.popup_scale-foto')
@@ -17,59 +18,17 @@ const figcaptionImg = popupOpenImag.querySelector('.popup__figire-title');
 // ------------------
 // форма и кнопка добавления новых карточек на страницу
 const btnEditCart = document.querySelector('.profile__button-add');
-const formEditCart = document.querySelector('.popup_add');
-const btnCloseEditCard = formEditCart.querySelector('.popup__container-close');
-const formHandlerAddCard = formEditCart.querySelector('.popup__wrapper-form');
-const nameNewCard = formEditCart.querySelector('.popup__form-input_type_text');
-const linkNewCard = formEditCart.querySelector('.popup__form-input_type_link');
+const popUpEditCart = document.querySelector('.popup_add');
+const btnCloseEditCard = popUpEditCart.querySelector('.popup__container-close');
+const formHandlerAddCard = popUpEditCart.querySelector('.popup__wrapper-form');
+const nameNewCard = popUpEditCart.querySelector('.popup__form-input_type_text');
+const linkNewCard = popUpEditCart.querySelector('.popup__form-input_type_link');
 // ------------------
+const templates = document.querySelector('.template').content;
 // данные пользователя на странице html
 const nameTitle = document.querySelector('.profile__info-title');
 const profileDescription = document.querySelector('.profile__discription')
 const containerCardItems = document.querySelector('.items-foto');
-// ------------------
-// Универсальная функции для откр/закр popup
-function openPopup (popupElement) {
-  popupElement.classList.add('popup_opened')
-  nameDescriptionSite();
-}
-function closePopup (popupElement) {
-  popupElement.classList.remove('popup_opened')
-}
-// ------------------
-
-// Слушатели событий для открытия формы редактирования данных
-btnInfoUser.addEventListener('click', function(evt) {
- openPopup(formPopapUser);
-})
-
-btnCloseFormUser.addEventListener('click', function(evt) {
-  closePopup(formPopapUser);
-})
-// ------------------
-// отображение имени и описания
-function nameDescriptionSite() {
-  nameUser.value = nameTitle.textContent;
-  discriptionUser.value = profileDescription.textContent;
-}
-// ------------------
-// Изменения данных имени пользователя из формы
-function handlerFormUserSubmit(evt) {
-  evt.preventDefault();
-  nameTitle.textContent = nameUser.value;
-  profileDescription.textContent = discriptionUser.value;
-  closePopup(formPopapUser);
-}
-formUserInfo.addEventListener('submit', handlerFormUserSubmit)
-// ------------------
-// сдушатели событий открытия формы для добавления карточек
-btnEditCart.addEventListener('click', () =>{
-  openPopup(formEditCart);
-})
-btnCloseEditCard.addEventListener('click', () => {
-  closePopup(formEditCart);
-})
-// ------------------
 const initialCards = [
   {
     name: 'Архыз',
@@ -96,58 +55,105 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+initialCards.forEach(x => {
+  const card = createCard(x);
+  renderCard(card);
+});
+// ------------------
+// Универсальная функции для откр/закр popup
+function openPopup (popupElement) {
+  popupElement.classList.add('popup_opened')
+  nameDescriptionSite();
+}
+function closePopup (popupElement) {
+  popupElement.classList.remove('popup_opened')
+}
+function openPropfilePopup(popupElement) {
+  popupElement.classList.remove('popup_opened')
+  nameDescriptionSite();
+  }
+// ------------------
+// отображение имени и описания
+function nameDescriptionSite() {
+  nameUser.value = nameTitle.textContent;
+  discriptionUser.value = profileDescription.textContent;
+}
+// ------------------
+// Изменения данных имени пользователя из формы
+function handlerFormUserSubmit(evt) {
+  evt.preventDefault();
+  nameTitle.textContent = nameUser.value;
+  profileDescription.textContent = discriptionUser.value;
+  closePopup(PopapUser);
+}
+// ------------------
 
-initialCards.forEach(createCard);
-
-
-// слушатели событий удаления. лайка. открытия попап с картинкой.
+// ------------------
 function btnLikeHandler(evt) {
   evt.target.classList.toggle('card__like');
 }
 function btnDeletHandler(evt) {
   evt.target.closest(".card").remove();
 }
-function btnOpenPopupImg (evt) {
-  imgPopup.src = evt.target.src;
-  figcaptionImg.textContent = evt.target.alt;
-  imgPopup.alt = evt.target.alt;
-  openPopup(popupOpenImag);
-}
-btnClosePopupImg.addEventListener('click', () => {
-  closePopup(popupOpenImag);
-})
-
-// ------------------
-// Функция создания карточек на страницу
+// Функция создания карточек
 function createCard (item) {
+  const templateCard = templates.querySelector('.card').cloneNode(true);
+  const nameCard = templateCard.querySelector('.card__name-title');
+  const imgCard = templateCard.querySelector('.card__imag');
+  const btnDeletCard = templateCard.querySelector('.card__button-remove');
+  const btnLikeCard = templateCard.querySelector('.card__name-heart');
   const name = item.name;
   const link = item.link;
-  const templates = document.querySelector('.template').content.querySelector('.card').cloneNode(true);
-  const nameCard = templates.querySelector('.card__name-title');
-  const imgCard = templates.querySelector('.card__imag');
-  const btnDeletCard = templates.querySelector('.card__button-remove');
-  const btnLikeCard = templates.querySelector('.card__name-heart');
   btnLikeCard.addEventListener('click', btnLikeHandler);
   btnDeletCard.addEventListener('click', btnDeletHandler);
   imgCard.addEventListener('click', btnOpenPopupImg);
   nameCard.textContent = name;
   imgCard.src = link;
   imgCard.alt = name;
-  containerCardItems.prepend(templates);
+  return templateCard;
 }
 // ------------------
+function renderCard(card) {
+  containerCardItems.prepend(card);
+}
 // Функция создания карточек
 function formCardHandler(evt) {
   evt.preventDefault();
-  createCard({link: linkNewCard.value, name: nameNewCard.value});
-  closePopup(formEditCart);
+  const newCard = createCard({link: linkNewCard.value, name: nameNewCard.value});
+  renderCard(newCard);
+  closePopup(popUpEditCart);
   evt.target.reset();
 }
 // ------------------
-formHandlerAddCard.addEventListener("submit", formCardHandler);
 
-// const templates = document.querySelector('.template').content.querySelector('.card').cloneNode(true);
-// ----
-  // const item = {link: linkNewCard.value, name: nameNewCard.value};
-  // createCard(item);
-  // ----
+function btnOpenPopupImg (evt) {
+  imgPopup.src = evt.target.src;
+  figcaptionImg.textContent = evt.target.alt;
+  imgPopup.alt = evt.target.alt;
+  openPopup(popupOpenImag);
+}
+
+formHandlerAddCard.addEventListener("submit", formCardHandler);
+btnClosePopupImg.addEventListener('click', () => {
+  closePopup(popupOpenImag);
+})
+
+// Слушатели событий для открытия формы редактирования данных
+btnInfoUser.addEventListener('click', function(evt) {
+  openPopup(PopapUser);
+ })
+
+ btnCloseFormUser.addEventListener('click', function(evt) {
+   closePopup(PopapUser);
+ })
+ // слушатели событий удаления. лайка. открытия попап с картинкой.
+
+formUserInfo.addEventListener('submit', handlerFormUserSubmit)
+// ------------------
+// сдушатели событий открытия формы для добавления карточек
+btnEditCart.addEventListener('click', () =>{
+  openPopup(popUpEditCart);
+})
+btnCloseEditCard.addEventListener('click', () => {
+  closePopup(popUpEditCart);
+})
